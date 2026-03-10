c5f8b252fd83263d1a30c4aadaf9fa9312e413b5
Neon <neon@saahild.com>
2026-03-09 23:28:00 -0400

    update: PLEASE RAILS

--- backend/config/environments/production.rb
+++ backend/config/environments/production.rb
@@ -82,11 +82,11 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  # config.hosts = [
-  #   "example.com",     # Allow requests from example.com
-  #   /.*\.example\.com/ # Allow requests from subdomains like `www.example.com`
-  # ]
+   config.hosts = [
+     "saahild.com",     # Allow requests from example.com
+     /.*\.saahild\.com/ # Allow requests from subdomains like `www.example.com`
+   ]
   #
   # Skip DNS rebinding protection for the default health check endpoint.
-  # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+   config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 end
